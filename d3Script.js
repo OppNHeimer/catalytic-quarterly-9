6cb42b79fa09369bd8b15aa7b345acbccb81ab88
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2020-04-22 16:17:14 -0500

    svg nodes displaying images and text

--- d3Script.js
+++ d3Script.js
@@ -39,90 +39,111 @@ const data = {
 
 const drag = simulation => {
   function dragstarted(d) {
-    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-    d.fx = d.x;
-    d.fy = d.y;
+    if (!d3.event.active) simulation.alphaTarget(0.3).restart()
+    d.fx = d.x
+    d.fy = d.y
   }
   
   function dragged(d) {
-    d.fx = d3.event.x;
-    d.fy = d3.event.y;
+    d.fx = d3.event.x
+    d.fy = d3.event.y
   }
   
   function dragended(d) {
-    if (!d3.event.active) simulation.alphaTarget(0);
-    d.fx = null;
-    d.fy = null;
+    if (!d3.event.active) simulation.alphaTarget(0)
+    d.fx = null
+    d.fy = null
   }
   
   return d3.drag()
     .on("start", dragstarted)
     .on("drag", dragged)
-    .on("end", dragended);
+    .on("end", dragended)
 }
 
-const root = d3.hierarchy(data);
-const links = root.links();
-const nodes = root.descendants();
+const root = d3.hierarchy(data)
+const links = root.links()
+const nodes = root.descendants()
+const imageNodes = nodes.filter(node => node.data.img)
+const textNodes = nodes.filter(node => !node.data.img)
 
 const simulation = d3.forceSimulation(nodes)
   .force("link", d3.forceLink(links).id(d => d.id).distance(0).strength(1))
   .force("charge", d3.forceManyBody().strength(-50))
   .force("x", d3.forceX())
-  .force("y", d3.forceY());
+  .force("y", d3.forceY())
 
 const height = window.innerHeight - 6
 const width = window.innerWidth
 
-console.log(height)
 // const svg = d3.create("svg")
-//   .attr("viewBox", [-width / 2, -height / 2, width, height]);
+//   .attr("viewBox", [-width / 2, -height / 2, width, height])
 const body = d3.select('body')
 body.style("margin", "0").style("padding", "0")
 
 const svg = body.append('svg')
-  .attr("viewBox", [-width / 2, -height / 2, width, height]);
+  .attr("viewBox", [-width / 2, -height / 2, width, height])
 
 const link = svg.append("g")
   .attr("stroke", "#999")
   .attr("stroke-opacity", 0.6)
   .selectAll("line")
   .data(links)
-  .join("line");
+  .join("line")
 
-const node = svg.append("g")
-  .attr("fill", "#fff")
-  .attr("stroke", "#000")
-  .attr("stroke-width", 1.5)
-  .selectAll("circle")
-  .data(nodes)
-  .join("circle")
-  .attr("fill", d => d.children ? null : "#000")
-  .attr("stroke", d => d.children ? null : "#fff")
-  .attr("r", 3.5)
-  .call(drag(simulation));
+const svgImageNodes = svg.append("g")
+  .selectAll("image")
+  .data(imageNodes)
+  .join("image")
+  .attr("xlink:href", d => d.data.img)
+  .attr("height", 50)
+  .attr("width", 50)
+  .call(drag(simulation))
 
-node.append("title")
-  .text(d => d.data.name);
+const svgTextNodes = svg.append("g")
+  .selectAll("text")
+  .data(textNodes)
+  .join("text")
+  .text(d => d.data.name)
+  // .attr("height", 50)
+  // .attr("width", 50)
+  // .attr("font-size", 50)
+  // .attr("font-family", 50)
+  .attr("font-family", "sans-serif")
+  .attr("font-size", "10px")
+  .attr("fill", "red")
+  .call(drag(simulation))
+
+// node.append("text")
+//   .text(d => d.data.name)
+//   .attr("fill", "#000")
+//   .attr("height", 50)
+//   .attr("width", 50)
+//   .attr("x", d => d.x)
+//   .attr("y", d => d.y)
 
 simulation.on("tick", () => {
-link
+  link
     .attr("x1", d => d.source.x)
     .attr("y1", d => d.source.y)
     .attr("x2", d => d.target.x)
-    .attr("y2", d => d.target.y);
+    .attr("y2", d => d.target.y)
 
-node
-    .attr("cx", d => d.x)
-    .attr("cy", d => d.y);
-});
+  svgImageNodes
+    .attr("x", d => d.x - 25)
+    .attr("y", d => d.y - 25)
+  
+    svgTextNodes
+    .attr("x", d => d.x - 25)
+    .attr("y", d => d.y - 25)
+})
 
-// invalidation.then(() => simulation.stop());
+// invalidation.then(() => simulation.stop())
 
 // const body = document.getElementById('body')
 // console.log(svg)
 // body.append(svg.groups[0])
 
-// return svg.node();
+// return svg.node()
 
 // console.log(svg)
